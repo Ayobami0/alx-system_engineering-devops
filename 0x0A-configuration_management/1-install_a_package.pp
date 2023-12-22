e922702f5e656f1b5a77e7a55782f13f01170a57
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-12-22 14:39:22 +0100

    fix: checker error

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 # Downloads the package flask using pip3
 package {'Flask':
-  provider        => 'pip3',
-  name            => 'flask==2.1.0',
-  install_options => ['install', '--ignore-installed']
+  provider => 'pip3',
+  name     => 'flask==2.1.0',
 }
