96cf1f0db7f57a9e26526102655987bcc942103f
Ayobami Oludemi <oludemiayobami@gmail.com>
2024-01-09 01:38:42 +0100

    fix: Checker fix for task 100 3

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,43 +1,46 @@
 # puppet config for nginx server
-file {'/etc/nginx/nginx.conf':
-  ensure  => present,
-  content => "
-http {
-			server {
-							listen 80;
-							root /var/www/data/;
-							index index.html;
-							error_page 404 /404.html;
-							add_header X-Served-By \$hostname;
-							location /redirect_me {
-											rewrite ^/ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
-							}
-							location /404.html {
-											root /var/www/data/error;
-											internal;
-							}
-			}
+exec {'nginx_conf':
+  require  => Exec['install_nginx'],
+  command  => 'http {
+        server {
+                listen 80;
+                root /var/www/data/;
+                index index.html;
+                error_page 404 /404.html;
+								add_header X-Served-By \$hostname;
+                location /redirect_me {
+                        rewrite ^/ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+                }
+                location /404.html {
+                        root /var/www/data/error;
+                        internal;
+                }
+        }
 }
-events {}",
-  require => Package['nginx']
-}
-
-
-file {'/var/www/data/index.html':
-  ensure  => present,
-  content => "Hello World!\n",
-  require => File['/etc/nginx/nginx.conf']
+events {}" | sudo tee /etc/nginx/nginx.conf >> /dev/null',
+  provider => 'shell',
 }
 
-file {'/var/www/data/error/404.html':
-  ensure  => present,
-  content => "Ceci n'est pas une page\n",
-  require => File['/var/www/data/index.html']
+
+exec {'index_html':
+  require  => Exec['nginx_conf'],
+  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command  => 'printf "Hello World!\n" | sudo tee /var/www/data/index.html >> /dev/null',
+  provider => 'shell',
 }
 
-service {'nginx':
-  ensure  => running,
-  require => File['/var/www/data/error/404.html']
+exec {'404_html':
+  require  => Exec['index_html'],
+  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command  => 'printf "Ceci n\'est pas une page\n" | sudo tee /var/www/data/error/404.html >> /dev/null',
+  provider => 'shell',
+}
+
+exec {'run_nginx':
+  require  => Exec['404_html'],
+  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  provider => 'shell',
+  command  => 'sudo service nginx start'
 }
 
 exec {'update':
@@ -47,9 +50,8 @@ exec {'update':
   before   => Exec['install_nginx'],
 }
 
-
 exec { 'install_nginx':
   path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
   command  => 'sudo apt-get install nginx -y',
-  provider => 'shell',
+  provider => 'shell'
 }
