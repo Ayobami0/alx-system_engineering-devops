7034849887af5ef9fa1321de5db9039a46563aa7
Ayobami Oludemi <oludemiayobami@gmail.com>
2024-02-23 00:04:45 +0100

    feat: Webstack Debugging

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# Sky is the limit, let's bring that limit higher 
+
+exec {'raise open files limit':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
